2b5fe63860a9092f7137185116bf43c00064c2a8
ginaperez <gmperez92@gmail.com>
2019-11-05 09:49:38 -0700

    gina perez changes

--- src/components/AuthComponent.js
+++ src/components/AuthComponent.js
@@ -7,11 +7,48 @@ import axios from 'axios';
 class AuthComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            register: true
+        };
+        this.register = this.register.bind(this);
+        this.login = this.login.bind(this);
     }
+    async register() {
+        const { email, password, firstName, lastName } = this.state;
+        const registeredUser = await axios.post('/auth/login', {
+            firstName,
+            lastName,
+            email,
+            password
+        });
+        this.props.setUser(registeredUser.data);
+    }
+    async login() {
+        const { email, password } = this.state;
+        const loggedInUser = await axios.post('/auth/login', {
+            email,
+            password
+        });
+        this.props.setUser(loggedInUser.data);
+    }
+
     render() {
         return <div>AuthComponent</div>
     }
 };
 
-export default AuthComponent;
+function mapReduxStateToProps(reduxState) {
+    return reduxState;
+}
+
+const mapDispatchToProps = {
+    setUser
+}
+
+const enhancedComponent = connect(mapReduxStateToProps, mapDispatchToProps);
+
+export default enhancedComponent(AuthComponent);
