234abd3cdc2dee7d1e079745f59652cefd2a3316
ginaperez <gmperez92@gmail.com>
2019-11-10 18:05:16 -0700

    gina perez chges

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
-import cartReducer from './cartReducer';
+import reducer from './reducer';
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
-
-const store = createStore(cartReducer)
 
 export default createStore(reducer, applyMiddleware(promiseMiddleware));
