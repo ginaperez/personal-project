c44f9baab23d3745d76bbe41d11eaa197a45958f
ginaperez <gmperez92@gmail.com>
2020-01-03 19:06:37 -0700

    gina perez changes

---
+++ build/precache-manifest.701f2710a0763c72a24dafed9a5f47d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afaa30b42fff82dc662b27be3ac6801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cea7b87eabf24ee53b3",
+    "url": "/static/css/main.e5cea7e1.chunk.css"
+  },
+  {
+    "revision": "aa72cc617a6583b4fcdd",
+    "url": "/static/js/2.265b2d85.chunk.js"
+  },
+  {
+    "revision": "8cea7b87eabf24ee53b3",
+    "url": "/static/js/main.c0accb03.chunk.js"
+  },
+  {
+    "revision": "8302febda05238bb8564",
+    "url": "/static/js/runtime-main.0a72e7f0.js"
+  }
+]);
